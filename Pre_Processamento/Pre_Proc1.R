03e7e0811adf7658d576b062586bf3b653114791
jhonyfuza <60222796+jhonyfuza@users.noreply.github.com>
2021-06-23 08:38:37 -0300

    Arquivos de pre-processamento

---
+++ Pre_Processamento/Pre_Proc1.R
@@ -0,0 +1,11 @@
+sum(2,7,5)
+
+x
+
+sum(x)    # if any element is NA or NaN, result is NA or NaN
+
+sum(x, na.rm=TRUE)    # this way we can ignore NA and NaN values
+
+mean(x, na.rm=TRUE)
+
+prod(x, na.rm=TRUE)
